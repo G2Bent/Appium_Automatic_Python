8fb7f01a970b62fa6aab8be6ff700360428b268e
origin <944921374@qq.com>
2018-05-29 00:08:57 +0800

    修改获取路径方法

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 import os,unittest,time
 from Report.HTMLTestRunner3 import HTMLTestRunner
 import sys
+
 def create_suite():
     TestSuite = unittest.TestSuite()#测试集
-    test_dir = os.getcwd()+'\\TestCase\\'
+    # test_dir = os.getcwd()+'\\TestCase\\'
+    test_dir = os.path.realpath('TestCase')
 
     discover = unittest.defaultTestLoader.discover(
         start_dir=test_dir,
-        pattern='Register.py',
+        pattern='test_*.py',
         top_level_dir=None
     )
 
@@ -18,21 +20,22 @@ def create_suite():
     return TestSuite
 
 def report():
-    if len(sys.argv) > 1:
-        report_name = os.getcwd() + '\\Report\\' + sys.argv[1] + '_result.html'
-    else:
-        now = time.strftime("%Y-%m-%d %H_%M_%S")
-        report_name = os.getcwd()+'\\Report\\result.html'
+    now = time.strftime("%Y-%m-%d %H_%M_%S")
+    # report_name = os.getcwd()+'\\Report\\result.html'
+    report_name = os.path.join(os.path.realpath("Report"), 'result.html')
     return report_name
 
 
 if __name__ == '__main__':
-    TestSuite = create_suite()
+
     fp = open(report(), 'wb')
+
+    TestSuite = create_suite()
     Runner = HTMLTestRunner(
         stream=fp,
         title='测试报告',
         description='测试用例执行情况'
     )
     Runner.run(TestSuite)
+
     fp.close()
